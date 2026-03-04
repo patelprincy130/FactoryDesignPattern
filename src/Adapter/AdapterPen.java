6e118a75d188baf5366dad68c8548b8388b2c3c7
Princy Patel <patelprincy130@gmail.com>
2026-03-04 05:43:03 -0500

    comment added

--- src/Adapter/AdapterPen.java
+++ src/Adapter/AdapterPen.java
@@ -2,7 +2,7 @@ package Adapter;
 
 public class AdapterPen implements Pen{
 
-    BallPen ballPen=new BallPen();
+    BallPen ballPen=new BallPen(); //using BallPen's implementation with composition
     @Override
     public void write(String str) {
         ballPen.writeWithBallPen(str);
